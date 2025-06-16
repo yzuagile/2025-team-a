01b88d9cb35f3fd20f0caa6e6b67e811868c43ad
unknown <lilian321123@gmail.com>
2025-06-17 03:09:01 +0800

    adding upgrade selections&GameStateManager&GameEventManager&mainMenu
    # Conflicts:
    #	Assets/Scenes/GameScene.unity
    #	Assets/Scripts/Enemy/EnemyStats.cs

--- Assets/Scripts/Pickups/PowerUpData.cs
+++ Assets/Scripts/Pickups/PowerUpData.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New PowerUpData", menuName = "Pickups/PowerUp Data", order = 53)]
 public class PowerUpData : ScriptableObject
 {
-    [Header("基本資訊")]
+    [Header("")]
     public PowerUpType type = PowerUpType.None;
-    public string powerUpName = "新道具";
-    public Sprite icon; // (可選) 道具在地面上或UI上的圖示
+    public string powerUpName = "speed";
+    public Sprite icon;
 
-    [Header("效果參數")]
-    public float duration = 5f;  // Buff 的持續時間 (秒)
-    public float value1 = 0f;    // 效果數值 (例如：增加的速度值、增加的傷害值)
-    public float value2 = 0f;    // (可選) 備用數值
+    [Header("")]
+    public float duration = 5f;
+    public float value1 = 0f;
+    public float value2 = 0f;
 
-    [Header("視覺 Prefab")]
-    public GameObject pickupPrefab; // 對應的掉落物預製件
+    [Header("Prefab")]
+    public GameObject pickupPrefab;
 }
